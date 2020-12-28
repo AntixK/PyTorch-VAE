7a449e9b2946f41d94fbaa6bd2b503c300a4fa1f
GPla <36087062+GPla@users.noreply.github.com>
2020-12-28 17:03:58 +0100

    fix import and named arguments for  pl >= 1.1.12

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from models import *
 from experiment import VAEXperiment
 import torch.backends.cudnn as cudnn
 from pytorch_lightning import Trainer
-from pytorch_lightning.logging import TestTubeLogger
+from pytorch_lightning.loggers import TestTubeLogger
 
 
 parser = argparse.ArgumentParser(description='Generic runner for VAE models')
@@ -42,13 +42,12 @@ experiment = VAEXperiment(model,
                           config['exp_params'])
 
 runner = Trainer(default_save_path=f"{tt_logger.save_dir}",
-                 min_nb_epochs=1,
+                 min_epochs=1,
                  logger=tt_logger,
-                 log_save_interval=100,
-                 train_percent_check=1.,
-                 val_percent_check=1.,
+                 flush_logs_every_n_steps=100,
+                 limit_train_batches=1.,
+                 limit_val_batches=1.,
                  num_sanity_val_steps=5,
-                 early_stop_callback = False,
                  **config['trainer_params'])
 
 print(f"======= Training {config['model_params']['name']} =======")
